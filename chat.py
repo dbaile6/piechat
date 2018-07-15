c0d942ef42762815c07d6e1df7b66aa9808ff6a9
Dylan Bailey <dylanba1@gmail.com>
2018-07-14 22:04:35 -0500

    Added initialization

--- chat.py
+++ chat.py
@@ -7,3 +7,13 @@ BUFSIZ = 1024
 ADDR = (HOST, PORT)
 SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDR)
+
+def accept_incoming_connections():
+    """Sets up handling for incoming clients."""
+    while True:
+        client, client_address = SERVER.accept()
+        print("%s:%s has connected." % client_address)
+        client.send(bytes("Howdy!"+
+                          "Pick a username and press the enter button", "utf8"))
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client,)).start()
