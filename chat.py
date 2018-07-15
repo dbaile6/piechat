a2d0e437109292da003cdf35ef8636c4d57696db
Dylan Bailey <dylanba1@gmail.com>
2018-07-15 05:59:17 -0500

    Gets names

--- chat.py
+++ chat.py
@@ -17,3 +17,22 @@ def accept_incoming_connections():
                           "Pick a username and press the enter button", "utf8"))
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
+
+        def handle_client(client):  # Takes client socket as argument.
+    """Handles a single client connection."""
+     name = client.recv(BUFSIZ).decode("utf8")
+    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
+     while True:
+        msg = client.recv(BUFSIZ)
+        if msg != bytes("{quit}", "utf8"):
+            broadcast(msg, name+": ")
+        else:
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            break
